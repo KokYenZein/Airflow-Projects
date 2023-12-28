f8308155cddec8f45f0926ea3d697667cba87f8b
KokYenZein <kokyenzein@gmail.com>
2023-12-28 00:17:51 -0500

    my_dag update

--- Airflow DAG/dags/my_dag.py
+++ Airflow DAG/dags/my_dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflows.operators.bash import BashOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 from random import randint
 
