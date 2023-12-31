a29e10e155eceea5e05d058b8197eb15b5c004a5
KokYenZein <kokyenzein@gmail.com>
2023-12-31 00:42:17 -0500

    Improved DAG
    utilized dynamic task mapping and decorators

--- Airflow DAG/dags/my_dag.py
+++ Airflow DAG/dags/my_dag.py
@@ -1,50 +1,30 @@
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 from random import randint
 
-# function that returns a random accuracy score
-def _training_model():
-    return randint(1, 10)
-
-def _choose_best_model(ti):
-    accuracies = ti.xcom_pull(task_ids=[
-        'training_model_A',
-        'training_model_B',
-        'training_model_C'
-    ])
-    best_accuracy = max(accuracies)
-    if (best_accuracy > 8):
-        return 'accurate'
-    else:
-        return 'inaccurate'
-
-
 with DAG("my_dag", 
          start_date = datetime(2021, 1, 1),
-         schedule_interval="@daily",
+         schedule = "@daily",
+         description = "Training Machine Learning Models",
+         tags = ["data engineering", "YZ"],
          catchup=False) as dag:
     
-    training_model_A = PythonOperator(
-        task_id = "training_model_A",
-        python_callable = _training_model
-    )
-
-    training_model_B = PythonOperator(
-        task_id = "training_model_B",
-        python_callable = _training_model
-    )
-
-    training_model_C = PythonOperator(
-        task_id = "training_model_C",
-        python_callable = _training_model
-    )
-
-    choose_best_model = BranchPythonOperator(
-        task_id = "choose_best_model",
-        python_callable = _choose_best_model
-    )
+    @task # decorator
+    # function that returns a random accuracy score
+    # utilizing dynamic task mapping
+    def training_model(accuracy):
+        return accuracy
+    
+    @task.branch # decorator
+    def choose_best_model(accuracies):
+        best_accuracy = max(accuracies)
+        if (best_accuracy > 8):
+            return 'accurate'
+        else:
+            return 'inaccurate'
 
     accurate = BashOperator(
         task_id = "accurate",
@@ -56,4 +36,5 @@ with DAG("my_dag",
         bash_command = "echo 'inaccurate'"
     )
 
-    [training_model_A, training_model_B, training_model_C] >> choose_best_model >> [accurate, inaccurate]
+    # dynamic task mapping (create 3 tasks)
+    choose_best_model(training_model.expand(accuracy=[5, 10, 6])) >> [accuarte, inaccurate]
